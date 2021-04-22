a94c4864fbaff945d90352ae0e7fd4ceb3592521
Tu DD <tudd@fabbi.io>
2021-04-22 23:05:48 +0700

    init

---
+++ lib/src/pages/index.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import './call.dart';
+
+class IndexPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => IndexState();
+}
+
+class IndexState extends State<IndexPage> {
+  final _channelController = TextEditingController();
+
+  @override
+  void dispose() {
+    _channelController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Agora Flutter QuickStart'),
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          height: 400,
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: RaisedButton(
+                        onPressed: onJoin,
+                        child: Text('Join'),
+                        color: Colors.blueAccent,
+                        textColor: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> onJoin() async {
+    await _handleCameraAndMic(Permission.camera);
+    await _handleCameraAndMic(Permission.microphone);
+    await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => CallPage(),
+      ),
+    );
+  }
+
+  Future<void> _handleCameraAndMic(Permission permission) async {
+    final status = await permission.request();
+    print(status);
+  }
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,4 @@
+/// Define App ID and Token
+const APP_ID = "81f2808d6f504f8da5c1d1d68a999454";
+const Token = "00681f2808d6f504f8da5c1d1d68a999454IACnlX+7tipW7crtRt5UqsVHXoVWwGZJEfwINBsO4pP4do5JbdwAAAAAEACHBVD5k8mCYAEAAQCTyYJg";
+const ChanelName = "tudd";
